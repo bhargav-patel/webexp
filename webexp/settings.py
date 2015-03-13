431424fe5344b0e0eb860cf22e61f39b40900cc5
Bhargav Patel <bhargav079@gmail.com>
2015-03-13 18:59:50 +0530

    Added compressor to installed apps.Added STATICFILES_DIRS and TEMPLATE_DIR.

--- webexp/settings.py
+++ webexp/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'authentication',
+    'compressor',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -80,4 +82,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
+
