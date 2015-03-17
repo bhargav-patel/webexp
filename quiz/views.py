b85abac9bd665d41b957d51ab14190a5d7d519a5
Bhargav Patel <bhargav079@gmail.com>
2015-03-18 01:13:30 +0530

    Fixed no image error.

--- quiz/views.py
+++ quiz/views.py
@@ -38,9 +38,10 @@ def getquestion(request):
 		temp = {
 			'level':q.level,
 			'question':q.question,
-			'image':q.image.url,
 			'points':q.points,
 		}
+		if q.image:
+			temp['image']=q.image.url
 		data = json.dumps(temp)
 		return HttpResponse(data,content_type='application/json')
 	raise Http404
